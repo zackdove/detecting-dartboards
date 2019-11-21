412d0958dea4f36373a8ed18265f686bb3d6dfc1
zackdove <zdove@live.co.uk>
2019-11-21 18:21:56 +0000

    Finished f1 calculation

--- f1score_faces.cpp
+++ f1score_faces.cpp
@@ -30,10 +30,14 @@ vector<Rect> ground_truth(int filenum);
 Rect vect_to_rect(vector<int> vect);
 float iou(Rect A, Rect B);
 int calculate_all();
+float f1_score();
 
 /** Global variables */
 String cascade_name = "frontalface.xml";
 CascadeClassifier cascade;
+float TP = 0;
+float FP = 0;
+float RP = 0;
 
 /** @function main */
 int main( int argc, const char** argv ){
@@ -47,6 +51,8 @@ int main( int argc, const char** argv ){
 		detectAndDisplay( frame, stoi(imgnum) );
 		imwrite( "detected.jpg", frame );
 	}
+	std::cout << "TPs = " << TP << std::endl;
+	std::cout << "FPs = " << FP << std::endl;
 	return 0;
 }
 
@@ -58,11 +64,21 @@ int main( int argc, const char** argv ){
  		detectAndDisplay( frame, imgnum);
  		imwrite( "detected"+to_string(imgnum)+".jpg", frame );
 	 }
+	 float f1 = f1_score();
+	 std::cout << "f1 score = " << f1 << std::endl;
 	 return 0;
  }
 
+float f1_score(){
+	float precision = TP/(TP+FP);
+	std::cout << "precision  = " << precision << std::endl;
+	float recall  = TP/(RP);
+	std::cout << "recall  = " << recall << std::endl;
+	return 2*(precision*recall)/(precision+recall);
+}
+
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame, int imgnum){
+	void detectAndDisplay( Mat frame, int imgnum){
 	std::vector<Rect> detected_faces;
 	Mat frame_gray;
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
@@ -70,26 +86,26 @@ void detectAndDisplay( Mat frame, int imgnum){
 	cascade.detectMultiScale( frame_gray, detected_faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 	vector<Rect> truth_faces = ground_truth(imgnum);
 	printFaces(truth_faces);
-	float threshold = 0.75;
+	float threshold = 0.5;
 	int true_positives = 0;
 	int false_positives = 0;
-	int total_faces = 0;
 	//Calculate IOU for each
 	for( int i = 0; i < detected_faces.size(); i++ ){
 		rectangle(frame, Point(detected_faces[i].x, detected_faces[i].y), Point(detected_faces[i].x + detected_faces[i].width, detected_faces[i].y + detected_faces[i].height), Scalar( 0, 255, 0 ), 2);
 		for (int j = 0; j < truth_faces.size(); j++){
 			float iou_result = iou(detected_faces[i], truth_faces[j]);
-			std::cout << "IOU for ground truth face " << truth_faces[j] << " with detected face " << detected_faces[i] << " = " << iou_result << std::endl;
+			// std::cout << "IOU for ground truth face " << truth_faces[j] << " with detected face " << detected_faces[i] << " = " << iou_result << std::endl;
 			if (iou_result >= threshold){
 				true_positives++;
-			} else {
-				false_positives++;
 			}
 		}
 	}
-	std::cout << "TPs = " << true_positives << std::endl;
-	std::cout << "FPs = " << false_positives << std::endl;
-
+	TP += true_positives;
+	FP += detected_faces.size()-true_positives;
+	//Draw for groud truthh
+	for (int i = 0; i < truth_faces.size(); i++){
+		rectangle(frame, Point(truth_faces[i].x, truth_faces[i].y), Point(truth_faces[i].x + truth_faces[i].width, truth_faces[i].y + truth_faces[i].height), Scalar( 0, 0, 255 ), 2);
+	}
 }
 
 void printFaces(std::vector<Rect> faces){
@@ -119,8 +135,9 @@ vector<Rect> ground_truth(int filenum){
         if (file2 == filenum) {
             count = 1;
 			row.erase(row.begin());
-			std::cout << "row size = " << row.size() << std::endl;
+			// std::cout << "row size = " << row.size() << std::endl;
 			for (int i=0; i<row.size(); i=i+4){
+				RP++;
 				face_coords_set.push_back(Rect(stoi(row[i]),stoi(row[i+1]),stoi(row[i+2])-stoi(row[i]),stoi(row[i+3])-stoi(row[i+1])));
 			}
 			return face_coords_set;
@@ -135,7 +152,6 @@ vector<Rect> ground_truth(int filenum){
 Rect vect_to_rect(vector<int> vect){
 	return Rect(vect[0], vect[1], vect[2]-vect[0], vect[3]-vect[1]);
 }
-
 
 float iou(Rect A, Rect B){
 	std::cout << "A " << A << std::endl;
