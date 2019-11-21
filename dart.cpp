473e5540adf27febcfe4004cb89aa83cdf133fa5
Jacob Ramaer <jr17327@it075695.wks.bris.ac.uk>
2019-11-21 16:45:53 +0000

    IOU Dartboard detection

--- dart.cpp
+++ dart.cpp
@@ -13,12 +13,14 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace cv;
 
 /** Function Headers */
-void detectAndDisplay( Mat frame );
+void detectAndDisplay( Mat frame , int imageNumber);
 
 /** Global variables */
 String cascade_name = "dart.xml";
@@ -28,14 +30,15 @@ CascadeClassifier cascade;
 /** @function main */
 int main( int argc, const char** argv )
 {
-       // 1. Read Input Image
+        // 1. Read Input Image
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	int imageNumber = atoi(argv[2]);
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-
+	
 	// 3. Detect Faces and Display Result
-	detectAndDisplay( frame );
+        detectAndDisplay( frame , imageNumber);
 
 	// 4. Save Result Image
 	imwrite( "detected.jpg", frame );
@@ -44,7 +47,7 @@ int main( int argc, const char** argv )
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
+void detectAndDisplay( Mat frame , int imageNumber)
 {
 	std::vector<Rect> faces;
 	Mat frame_gray;
@@ -56,13 +59,60 @@ void detectAndDisplay( Mat frame )
 	// 2. Perform Viola-Jones Object Detection 
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
-       // 3. Print number of Faces found
+	// Loading true position coordinates into matrix
+        vector<double> matrix;
+        fstream file;
+        file.open("dart.csv");
+        string line;
+        while (getline( file, line,'\n'))
+	{
+             istringstream templine(line); 
+             string data;
+	     while (getline( templine, data,',')) 
+	        {
+	           matrix.push_back(atof(data.c_str())); 
+                }
+	}
+        vector<double> coordinates;
+        for(int i = 0; i < matrix.size(); i = i+5)
+             {
+             if(matrix[i] == imageNumber)
+		{
+                   for(int j = 1; j < 5; j++)
+		     {
+		        coordinates.push_back(matrix[i+j]);
+                     }
+                }
+             }
+        file.close();
+        
+        //IOU
+        for(int j = 0; j < coordinates.size(); j = j+4){
+             Rect r1 = Rect(Point(coordinates[j], coordinates[j+1]), Point(coordinates[j+2], coordinates[j+3]));
+             float maxiou = 0;
+             for( int i = 0; i < faces.size(); i++ ) {
+                 Rect r2 = Rect(Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height));
+                 float is = (r1 & r2).area();
+                 float u = (r1 | r2).area();
+                 float iou = (double)is/u;
+                 if(iou > maxiou){
+                     maxiou = iou;
+                 }
+             }
+             // Draw RED box around true position 
+             rectangle(frame, Point(coordinates[i], coordinates[i+1]), Point(coordinates[i+2], coordinates[i+3]), Scalar( 0, 0, 255 ), 2);
+             // Print IOU
+             std::cout << "IOU for dartboard " << (j/4) + 1 << " = "<< maxiou << std::endl;
+        }
+
+        // 3. Print number of Faces found
 	std::cout << faces.size() << std::endl;
 
-       // 4. Draw box around faces found
+        // 4. Draw box around faces found
 	for( int i = 0; i < faces.size(); i++ )
 	{
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
+        
 
 }
