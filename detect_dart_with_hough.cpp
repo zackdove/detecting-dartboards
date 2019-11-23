6aced213b3057c78feda7d0a6c9245c38c013e43
Jacob Ramaer <jr17327@it075728.wks.bris.ac.uk>
2019-11-23 17:50:17 +0000

    added sobel

--- detect_dart_with_hough.cpp
+++ detect_dart_with_hough.cpp
@@ -2,6 +2,8 @@
 //
 // Detect dartboards
 //
+// g++ detect_dart_with_hough.cpp /usr/lib64/libopencv_core.so.2.4  /usr/lib64/libopencv_highgui.so.2.4  /usr/lib64/libopencv_imgproc.so.2.4  /usr/lib64/libopencv_objdetect.so.2.4 -std=c++11
+//
 /////////////////////////////////////////////////////////////////////////////
 
 // header inclusion
@@ -19,6 +21,8 @@
 #include <sstream>
 //For printing correct precision
 #include <iomanip>
+#include <iostream>  
+#include<string> 
 
 using namespace std;
 using namespace cv;
@@ -28,13 +32,15 @@ void detectAndDisplay( Mat frame, int imgnum );
 void printFaces(std::vector<Rect> faces);
 Rect vect_to_rect(vector<int> vect);
 int calculate_all();
+void sobel(cv::Mat &input, Mat_<float> kernel, int size, cv::Mat &convo);
+void MagDir(cv::Mat &inputx, cv::Mat &inputy, cv::Mat &mag, cv::Mat &dir);
 bool contains_circle(Mat frame);
 bool contains_line(Mat frame);
 bool contains_ellipse(Mat frame);
 bool contains_clustered_lines(Mat frame);
 
 /** Global variables */
-String cascade_name = "frontalface.xml";
+String cascade_name = "dart.xml";
 CascadeClassifier cascade;
 
 /** @function main */
@@ -57,7 +63,7 @@ int main( int argc, const char** argv ){
     string filename =  "dart"+to_string(imgnum)+".jpg";
     	Mat frame = imread(filename, CV_LOAD_IMAGE_COLOR);
     	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-    	detectAndDisplay( frame, imgnum);
+        detectAndDisplay( frame, imgnum);
     	imwrite( "detected"+to_string(imgnum)+".jpg", frame );
     }
     return 0;
@@ -67,6 +73,9 @@ bool contains_circle(Mat frame){
     //Calculate hough space for circle
     //Threshold
     //If above threshold, return true
+
+
+  
     return true;
 }
 
@@ -94,21 +103,41 @@ bool contains_clustered_lines(Mat frame){
 
 /** @function detectAndDisplay */
 	void detectAndDisplay( Mat frame, int imgnum){
-	std::vector<Rect> detected_faces;
-	Mat frame_gray;
-	cvtColor( frame, frame_gray, CV_BGR2GRAY );
-	equalizeHist( frame_gray, frame_gray );
-	cascade.detectMultiScale( frame_gray, detected_faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
-    cout << "Original detected faces" << endl;
-	printFaces(detected_faces);
-    for (int i = 0; i < detected_faces.size(); i++){
-        Mat face_frame = frame(detected_faces[i]);
+	std::vector<Rect> detected_dartboards;
+	Mat frame_grey;
+	cvtColor( frame, frame_grey, CV_BGR2GRAY );
+	equalizeHist( frame_grey, frame_grey );
+	cascade.detectMultiScale( frame_grey, detected_dartboards, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+    cout << "Original detected dartboards" << endl;
+	printFaces(detected_dartboards);
+    for (int i = 0; i < detected_dartboards.size(); i++){
+
+        Mat dartboard_frame = frame(detected_dartboards[i]);
         //Check if face_frame cotains circle, line, ellipse etc
         //If it doesnt meet the criteria, erase it
+	// create the SOBEL kernel in 1D, Y is transpose
+	Mat_<float> kernel(3,3);
+	kernel << 1, 0, -1, 2, 0, -2, 1, 0, -1; 
+
+	Mat xConvo;
+        sobel(dartboard_frame,kernel,23,xConvo);
+
+        Mat yConvo;
+        sobel(dartboard_frame,kernel.t(),23,yConvo);
+
+	Mat mag;
+	Mat dir;
+	MagDir(xConvo, yConvo, mag, dir);
+
+	 imwrite( "xConvo.jpg", xConvo );
+	 imwrite( "yConvo.jpg", yConvo );
+	 imwrite( "Mag.jpg", mag );
+	 imwrite( "Dir.jpg", dir );
+         
     }
 	//Draw for groud truthh
-	for (int i = 0; i < detected_faces.size(); i++){
-		rectangle(frame, Point(detected_faces[i].x, detected_faces[i].y), Point(detected_faces[i].x + detected_faces[i].width, detected_faces[i].y + detected_faces[i].height), Scalar( 0, 0, 255 ), 2);
+	for (int i = 0; i < detected_dartboards.size(); i++){
+		rectangle(frame, Point(detected_dartboards[i].x, detected_dartboards[i].y), Point(detected_dartboards[i].x + detected_dartboards[i].width, detected_dartboards[i].y + detected_dartboards[i].height), Scalar( 0, 0, 255 ), 2);
 	}
 }
 
@@ -116,4 +145,126 @@ void printFaces(std::vector<Rect> faces){
 	for (int i = 0; i < faces.size(); i++){
 		std::cout << faces[i] << std::endl;
 	}
+}
+
+void sobel(cv::Mat &input, Mat_<float> kernel, int size, cv::Mat &convo)
+{
+	// intialise the output using the input
+        convo.create(input.size(), CV_32FC1);
+
+	// we need to create a padded version of the input
+	// or there will be border effects
+	int kernelRadiusX = ( kernel.size[0] - 1 ) / 2;
+	int kernelRadiusY = ( kernel.size[1] - 1 ) / 2;
+
+	cv::Mat paddedInput;
+	cv::copyMakeBorder( input, paddedInput, 
+		kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+		cv::BORDER_REPLICATE );
+
+	float min = 1000;
+	float max = -1000;
+
+	for ( int i = 1; i < input.rows-1; i++ )
+	{	
+		for( int j = 1; j < input.cols-1; j++ )
+		{
+		        double sum = 0.0;
+
+			for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ )
+			{
+				for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ )
+				{
+					// find the correct indices we are using
+					int imagex = i + m + kernelRadiusX;
+					int imagey = j + n + kernelRadiusY;
+					int kernelx = m + kernelRadiusX;
+					int kernely = n + kernelRadiusY;
+					
+					// get the values from the padded image and the kernel
+					int imageval = ( int ) paddedInput.at<uchar>( imagex, imagey );
+					double kernalval = kernel.at<double>( kernelx, kernely );
+
+					// do the multiplication
+					sum += imageval * kernalval;
+					convo.at<float>(i, j) =  sum;
+				        
+				}
+			}
+			
+			// Finding min & max values (for normalisation)
+			if(sum > max) {
+			  max = sum;
+			}
+			if(sum < min) {
+			  min = sum;
+			}
+
+		}
+	}
+
+	// Normalising loop
+	for ( int i = 1; i < input.rows-1; i++ )
+	{	
+		for( int j = 1; j < input.cols-1; j++ )
+		{
+		        // Normalize calculation
+			float normSum = ((convo.at<float>(i, j) - min) * 255)/ (max - min);
+			convo.at<float>(i, j) =  normSum;
+		}
+	}
+}
+
+void MagDir(cv::Mat &inputx, cv::Mat &inputy, cv::Mat &mag, cv::Mat &dir)
+{
+  	// intialise the output using the input
+        mag.create(inputx.size(), CV_32FC1);
+	dir.create(inputx.size(), CV_32FC1);
+
+	float magmin = 1000;
+	float magmax = -1000;
+	float dirmin = 1000;
+	float dirmax = -1000;
+
+	for ( int i = 0; i < inputx.rows; i++ )
+	{	
+	  for( int j = 0; j < inputx.cols; j++ )
+		{
+
+		  float magx = inputx.at<float>(i, j);
+		  float magy = inputy.at<float>(i, j);
+		  float magTemp = sqrt(pow(magx, 2.0) + pow(magy, 2.0));
+                  float dirTemp = atan2(magy, magx);
+		  mag.at<float>(i, j) = magTemp;
+		  dir.at<float>(i, j) = dirTemp;		           
+
+		  // Finding min & max values (for normalisation)
+		  if(magTemp > magmax) {
+                        magmax = magTemp;
+		  }
+		  if(magTemp < magmin) {
+		 	magmin = magTemp;
+		  }
+                  if(dirTemp > dirmax) {
+                        dirmax = dirTemp;
+		  }
+		  if(dirTemp < dirmin) {
+		 	dirmin = dirTemp;
+		  }
+                        
+		}
+	}
+
+	// Normalising loop
+		for ( int i = 0; i < inputx.rows; i++ )
+	  	{	
+	  		for( int j = 0; j < inputx.cols; j++ )
+		  	{
+                                //Normalize calculation
+		  		float normMag = ((mag.at<float>(i, j) - magmin) * 255)/ (magmax - magmin);
+                                float normDir = ((dir.at<float>(i, j) - dirmin) * 255)/ (dirmax - dirmin);
+				mag.at<float>(i, j) =  normMag;
+                            	dir.at<float>(i, j) =  normDir;
+                        }		
+	       	}
 }
